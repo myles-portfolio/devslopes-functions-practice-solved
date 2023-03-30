340625b06f0f25035131f02293e4753315edfeae
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-03-30 12:50:26 -0500

    completed e4

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,10 +9,9 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
+const splitFirstAndLastNames = (str) => {
   return str.split(' ');
 }
-
 
 /** =========================
  * The personAge() constant is assigned to a function, that takes an object of person Data as the argument.
@@ -20,10 +19,9 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
+const personAge = (personObject) => {
   return personObject.age;
 }
-
 
 /** =========================
  * The isNameInArray function takes an array and string of name as the arguments.
@@ -32,10 +30,9 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
+const isNameInArray = (arr, name) => {
   return arr.includes(name);
 }
-
 
 /** =========================
  * The logTimer function takes a number as the arguments.
@@ -44,7 +41,7 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
   const timer = setInterval(function() {
     if (i < max) {
